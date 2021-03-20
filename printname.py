06d8e10aed47de872d958f05b6f82fba3bcfec47
Hammadrehmna <77200025+Hammadrehmna@users.noreply.github.com>
2021-03-20 20:27:11 +0500

    Add files via upload

---
+++ printname.py
@@ -0,0 +1,6 @@
+def name(name):
+    x=len(name)
+    print(x)
+def age(age):
+    x=age
+    print(x)
---
+++ restu.py
@@ -0,0 +1,13 @@
+class Restaurant():
+    def __init__(self,restaurant_name,cuisine_type):
+        self.restaurant_name = restaurant_name
+        self.cuisine_type = cuisine_type
+        self.number_served = 0
+    def describe_restaurant(self):
+        print(self.restaurant_name, self.cuisine_type)
+    def open_restaurant(self):
+        print("restaurant is open")
+        
+        
+res1 = Restaurant("ABCD restaurant", "indian")
+        
